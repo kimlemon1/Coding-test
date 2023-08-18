52347798a5bfd28abb0a6100a3e66415b05722bc
kimlemon1 <86584713+kimlemon1@users.noreply.github.com>
2023-08-18 10:07:20 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+//#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    unsigned short a = 0;
+    unsigned short b = 0;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+}
